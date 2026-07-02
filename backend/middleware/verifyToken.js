f0fc9a6a3a95d50bd90e6c0aa8870fd804130e52
Est3ban-JYP18 <thebrother680@gmail.com>
2026-07-01 22:28:24 -0500

    Seguridad en backend
    Signed-off-by: Est3ban-JYP18 <thebrother680@gmail.com>

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+
+function verifyToken(req, res, next) {
+  // Obtener el header Authorization
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return res.status(401).json({
+      message: "Acceso denegado. Token no proporcionado."
+    });
+  }
+
+  // Debe venir como: Bearer eyJhbGc...
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Token inválido."
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Guardamos los datos del usuario para usarlos después
+    req.usuario = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Token inválido o expirado."
+    });
+  }
+}
+
+module.exports = verifyToken;
